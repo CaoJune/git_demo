1a4b7ea48e425ad1f70ea2248bb066d567abec61
cjy <jinyanc@olymtech.com>
2014-10-21 10:47:43 +0800

    User account B commit

--- src/com/olymtech/git/Test.java
+++ src/com/olymtech/git/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println(">>>>Hello world !! >>>> git demo >>>>");
 		System.out.println(">>>>Hello world !! >>>> git demo >>>> first commit");
+		System.out.println(">>>> User account B commit ");
 	}
 
 }
