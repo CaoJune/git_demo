2dcba62f3d1a7592e452862e5e94b6feb69e5bac
cjy <jinyanc@olymtech.com>
2014-10-21 09:51:52 +0800

    git_demo first commit

---
+++ src/com/olymtech/git/Test.java
@@ -0,0 +1,11 @@
+package com.olymtech.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(">>>>Hello world !! >>>> git demo >>>>");
+		System.out.println(">>>>Hello world !! >>>> git demo >>>> first commit");
+	}
+
+}
