0263de5f23ec3166ab9b2c66b443dc53cdb93cf5
jyc <jyc@JYC-PC>
2014-10-24 15:52:22 +0800

    git_demo master brach 2nd commit

--- src/com/olymtech/git/Test.java
+++ src/com/olymtech/git/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		System.out.println(">>>>Hello world !! >>>> git demo >>>>");
 		System.out.println(">>>>Hello world !! >>>> git demo >>>> first commit");
 		System.out.println(">>>> User account B commit ");
+		System.out.println(">>>> git_demo master brach 2nd commit >>>> ");
 	}
 
 }
