a06ac3265e449c5152ee949436895dea42c42ea9
jyc <jyc@JYC-PC>
2014-10-24 16:15:01 +0800

    add class Mouse

---
+++ src/com/olymtech/git/Mouse.java
@@ -0,0 +1,20 @@
+package com.olymtech.git;
+
+public class Mouse {
+	private String type;
+	private String color;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
