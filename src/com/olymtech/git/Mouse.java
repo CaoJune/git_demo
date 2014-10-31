6911cfc7843dd1c687d3ccc6df80d87b2ff3d3bf
caojinyan <jinyanc@olymtech.com>
2014-10-31 14:42:37 +0800

    add Date function

--- src/com/olymtech/git/Mouse.java
+++ src/com/olymtech/git/Mouse.java
@@ -1,5 +1,8 @@
 package com.olymtech.git;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Mouse {
 	private String type;
 	private String color;
@@ -15,6 +18,14 @@ public class Mouse {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	
+	public static Date add(Date inputDate, int interval, int calendarUnit) {
+        if (inputDate == null) {
+            return null;
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(inputDate);
+        cal.add(calendarUnit, interval);
+        return cal.getTime();
+    }
 	
 }
